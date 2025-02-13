ad1276b5e50987fe4a74ffd8d30403bfd59d7b7f
fmandia <fmandia1201@gmail.com>
2025-02-13 19:58:42 +0100

    Auth service Singleton instance

--- lib/services/auth/auth_service.dart
+++ lib/services/auth/auth_service.dart
@@ -1,6 +1,22 @@
 
 import 'package:firebase_auth/firebase_auth.dart';
 
+//* This is a service that manages client authentication.
 class AuthService {
+  // Firebase auth is a class that acts as an access point to the Firebase Authentication SDK. 
+  final FirebaseAuth _auth;
+  // Controls whether the user is registered or not. May contain null if there is no registered user.
+  late final Stream<User?> _user;
+  // Singleton
+  static AuthService instance = AuthService._privateConstructor();
+
+  //We obtain the auth instance. Once obtained we initialize _user
+  AuthService._privateConstructor():
+    _auth = FirebaseAuth.instance {
+      _user = _auth.authStateChanges();
+    }
     
+  factory AuthService() {
+    return instance;
+  }
 }
