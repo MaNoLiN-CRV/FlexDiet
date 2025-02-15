756305a743e391c3b2fe30d956e9dfca01678178
fmandia <fmandia1201@gmail.com>
2025-02-15 17:20:14 +0100

    Implementing without any logic

---
+++ lib/screens/setting_screen.dart
@@ -0,0 +1,41 @@
+import 'package:flutter/material.dart';
+import 'package:flutter_flexdiet/widgets/widgets.dart';
+
+class SettingScreen extends StatelessWidget {
+  const SettingScreen({Key? key}) : super(key: key);
+
+  @override
+  Widget build(BuildContext context) {
+    final Size size = MediaQuery.of(context).size;
+    return Scaffold(
+        appBar: AppBar(
+          title: Text('Ajustes'),
+          actions: [Text('My Profile')],
+        ),
+        body: Column(children: [
+          Container(
+            padding: EdgeInsets.symmetric(vertical: size.width * 0.05, horizontal: size.height * 0.01),
+            child: Row(
+              children: [
+                // To do
+                CustomCircleAvatar(
+                  image: 'https://media.istockphoto.com/id/1090878494/es/foto/retrato-de-joven-sonriente-a-hombre-guapo-en-camiseta-polo-azul-aislado-sobre-fondo-gris-de.jpg?s=612x612&w=0&k=20&c=dHFsDEJSZ1kuSO4wTDAEaGOJEF-HuToZ6Gt-E2odc6U=',
+                  radius: size.width * 0.20,
+                ),
+                Padding(
+                  padding: EdgeInsets.only(left: size.height * 0.05),
+                  child: Column(
+                    children: [
+                      Text('Username Name')
+                    ],
+                  ),
+                )
+              ],
+            ),
+          ),
+          Container(
+            
+          )
+        ]));
+  }
+}
