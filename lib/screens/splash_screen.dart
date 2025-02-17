1971a6c7e4fe234dd0cf76350ad31466eedec532
MaNoLiN-CRV <manoliin.27@gmail.com>
2025-02-17 13:29:17 +0100

    Slogan added in splash screen with a gradient

--- lib/screens/splash_screen.dart
+++ lib/screens/splash_screen.dart
@@ -1,7 +1,6 @@
 import 'package:flutter/material.dart';
 import 'dart:async';
-import 'package:flutter_flexdiet/screens/login_screen.dart'; 
-
+import 'package:flutter_flexdiet/screens/login_screen.dart';
 
 class SplashScreen extends StatefulWidget {
   const SplashScreen({super.key});
@@ -18,16 +17,17 @@ class _SplashScreenState extends State<SplashScreen> {
   }
 
   _navigateToHome() async {
-    await Future.delayed(const Duration(seconds: 3), () { 
+    await Future.delayed(const Duration(seconds: 3), () {
       Navigator.pushReplacement(
         context,
-        MaterialPageRoute(builder: (context) => const LoginScreen()), 
+        MaterialPageRoute(builder: (context) => const LoginScreen()),
       );
     });
   }
 
   @override
   Widget build(BuildContext context) {
+    final theme = Theme.of(context);
     return Scaffold(
       backgroundColor: Colors.white,
       body: Center(
@@ -40,11 +40,33 @@ class _SplashScreenState extends State<SplashScreen> {
                 borderRadius: BorderRadius.circular(20),
                 child: Image.asset(
                   fit: BoxFit.contain,
-                  'assets/images/logo.png', 
-                  width: double.infinity, 
-                  height: 300,
-
+                  'assets/images/logo.png',
+                  width: 200,
+                  height: 200,
                 ),
+              ),
+            ),
+            const SizedBox(height: 24),
+            ShaderMask(
+              shaderCallback: (bounds) {
+                return LinearGradient(
+                  colors: [
+                    const Color.fromARGB(255, 28, 27, 27),
+                    const Color.fromARGB(255, 34, 29, 74) , 
+                    const Color.fromARGB(255, 41, 84, 154) , 
+                    const Color.fromARGB(255, 2, 81, 227)],
+                  begin: Alignment.bottomLeft,
+                  end: Alignment.topRight,
+                ).createShader(bounds);
+              },
+              child: Text(
+                'Tu camino hacia una vida saludable',
+                style: theme.textTheme.bodyLarge?.copyWith(
+                  color: Colors.white,
+                  fontSize: 20,
+              
+                ),
+                textAlign: TextAlign.center,
               ),
             ),
           ],
@@ -53,4 +75,3 @@ class _SplashScreenState extends State<SplashScreen> {
     );
   }
 }
-
