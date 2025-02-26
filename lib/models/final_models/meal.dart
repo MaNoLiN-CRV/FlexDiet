19351f2536e8add9c6a95b05433f9b1bdf0c353e
MaNoLiN-CRV <manoliin.27@gmail.com>
2025-02-26 09:28:38 +0100

    Meal model implemented

---
+++ lib/models/final_models/meal.dart
@@ -0,0 +1,64 @@
+import 'package:cloud_firestore/cloud_firestore.dart';
+import 'package:flutter_flexdiet/firebase_firestore.dart';
+
+final firestore = FirestoreService.firestore;
+
+class Meal {
+  String id; // UUID
+  String? name;
+  String? protein;
+  String? carbs;
+  String? image;
+
+  Meal({
+    required this.id,
+    this.name,
+    this.protein,
+    this.carbs,
+    this.image,
+  });
+
+  Map<String, dynamic> toJson() {
+    return {
+      'name': name,
+      'protein': protein,
+      'carbs': carbs,
+      'image': image,
+    };
+  }
+
+  factory Meal.fromJson(Map<String, dynamic> json, String id) {
+    return Meal(
+      id: id,
+      name: json['name'],
+      protein: json['protein'],
+      carbs: json['carbs'],
+      image: json['image'],
+    );
+  }
+
+  // Métodos Firestore para Meal
+  static CollectionReference<Meal> get collection =>
+      firestore.collection('meals').withConverter<Meal>(
+            fromFirestore: (snapshot, _) =>
+                Meal.fromJson(snapshot.data()!, snapshot.id),
+            toFirestore: (meal, _) => meal.toJson(),
+          );
+
+  static Future<Meal?> getMeal(String mealId) async {
+    final docSnap = await collection.doc(mealId).get();
+    return docSnap.data();
+  }
+
+  static Future<void> createMeal(Meal meal) async {
+    await collection.doc(meal.id).set(meal);
+  }
+
+  static Future<void> updateMeal(Meal meal) async {
+    await collection.doc(meal.id).update(meal.toJson());
+  }
+
+  static Future<void> deleteMeal(String mealId) async {
+    await collection.doc(mealId).delete();
+  }
+}
