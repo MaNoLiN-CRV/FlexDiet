51d6e50583e7d89054c96fc6ec009bb0f3225721
Alejandro Hortal Valor <alejandrohortalvalor02@gmail.com>
2025-02-27 14:26:53 +0100

    ForgotPasswordScreen done

--- lib/screens/admin/edit_person_screen.dart
+++ lib/screens/admin/edit_person_screen.dart
@@ -1,12 +1,11 @@
 import 'package:flutter/material.dart';
 import 'package:flutter_flexdiet/models/final_models/client.dart';
 import 'package:flutter_animate/flutter_animate.dart';
-import 'package:flutter_flexdiet/widgets/custom_toast.dart'; // Assuming you have a custom toast widget
+import 'package:flutter_flexdiet/widgets/custom_toast.dart';
 
 class EditPerson extends StatefulWidget {
-  late Client client;
   final String clientId;
-  EditPerson({super.key, required this.clientId});
+  const EditPerson({super.key, required this.clientId});
 
   @override
   State<EditPerson> createState() => _EditPersonState();
@@ -19,33 +18,38 @@ class _EditPersonState extends State<EditPerson> {
   late final TextEditingController descriptionController;
   late final TextEditingController heightController;
   String? sex;
+  Client? client;
 
   @override
   void initState() {
     super.initState();
     _initializeData();
-    _initializeControllers();
   }
 
   void _initializeData() async {
-    final Client? client = await Client.getClient(widget.clientId);
-    if (client != null && mounted) {
-      setState(() => widget.client = client);
+    final Client fetchedClient = await Client.getClient(widget.clientId);
+    if (mounted) {
+      setState(() {
+        client = fetchedClient;
+        _initializeControllers();
+      });
     } else {
-      showToast(context, 'Cliente no encontrado', toastType: ToastType.error);
-      Navigator.pop(context);
+      WidgetsBinding.instance.addPostFrameCallback((_) {
+        showToast(context, 'Cliente no encontrado', toastType: ToastType.error);
+        Navigator.pop(context);
+      });
     }
   }
 
   void _initializeControllers() {
-    nameController = TextEditingController(text: widget.client.username);
+    nameController = TextEditingController(text: client?.username);
     kgController =
-        TextEditingController(text: widget.client.bodyweight?.toString() ?? '');
+        TextEditingController(text: client?.bodyweight?.toString() ?? '');
     descriptionController =
-        TextEditingController(text: widget.client.description ?? '');
+        TextEditingController(text: client?.description ?? '');
     heightController =
-        TextEditingController(text: widget.client.height?.toString() ?? '');
-    sex = widget.client.sex;
+        TextEditingController(text: client?.height?.toString() ?? '');
+    sex = client?.sex;
   }
 
   @override
@@ -112,16 +116,16 @@ class _EditPersonState extends State<EditPerson> {
     if (result == true && mounted) {
       final bool deleteSuccess = await Client.deleteClient(widget.clientId);
       if (deleteSuccess) {
-        ScaffoldMessenger.of(context).showSnackBar(
-          const SnackBar(
-            backgroundColor: Colors.red,
-            content: Text('Cliente eliminado'),
-            behavior: SnackBarBehavior.floating,
-          ),
-        );
-        Navigator.pop(context);
+        if (mounted) {
+          showToast(context, 'Cliente eliminado correctamente',
+              toastType: ToastType.error);
+          Navigator.pop(context);
+        }
       } else {
-        showToast(context, 'Error al eliminar el cliente', toastType: ToastType.error);
+        if (mounted) {
+          showToast(context, 'Error al eliminar el cliente',
+              toastType: ToastType.error);
+        }
       }
     }
   }
--- lib/screens/login/forgot_password_screen.dart
+++ lib/screens/login/forgot_password_screen.dart
@@ -1,5 +1,9 @@
+import 'package:firebase_auth/firebase_auth.dart';
 import 'package:flutter/material.dart';
 import 'package:flutter_flexdiet/theme/theme.dart';
+import 'package:flutter_flexdiet/widgets/custom_toast.dart';
+import 'package:uni_links/uni_links.dart';
+import 'dart:async';
 
 class ForgotPasswordScreen extends StatefulWidget {
   const ForgotPasswordScreen({super.key});
@@ -13,6 +17,8 @@ class _ForgotPasswordScreenState extends State<ForgotPasswordScreen>
   late AnimationController _animationController;
   late Animation<Color?> _backgroundColorAnimation;
   final TextEditingController _emailController = TextEditingController();
+  final FirebaseAuth _auth = FirebaseAuth.instance;
+  StreamSubscription? _sub;
 
   @override
   void initState() {
@@ -26,13 +32,166 @@ class _ForgotPasswordScreenState extends State<ForgotPasswordScreen>
       begin: backgroundColorWhite,
       end: const Color.fromARGB(48, 98, 15, 231),
     ).animate(_animationController);
+
+    _handleIncomingLinks();
   }
 
   @override
   void dispose() {
     _animationController.dispose();
     _emailController.dispose();
+    _sub?.cancel();
     super.dispose();
+  }
+
+  void _handleIncomingLinks() {
+    _sub = uriLinkStream.listen((Uri? uri) {
+      if (uri != null && uri.path == '/reset-password') {
+        _showResetPasswordDialog();
+      }
+    }, onError: (Object err) {
+      // Handle error
+    });
+  }
+
+  Future<void> _sendPasswordResetEmail() async {
+    final email = _emailController.text.trim();
+
+    if (email.isEmpty) {
+      showToast(context, "Por favor, introduce un correo electrónico",
+          toastType: ToastType.error);
+      return;
+    }
+
+    try {
+      await _auth.sendPasswordResetEmail(email: email);
+      if (mounted) {
+        showToast(context, "Correo electrónico enviado",
+            toastType: ToastType.success);
+      }
+    } on FirebaseAuthException catch (e) {
+      if (mounted) {
+        String errorMessage;
+        switch (e.code) {
+          case 'invalid-email':
+            errorMessage = 'El formato del correo electrónico es inválido.';
+            break;
+          case 'user-not-found':
+            errorMessage = 'No hay ninguna cuenta registrada con este correo.';
+            break;
+          default:
+            errorMessage = 'Ocurrió un error inesperado. Inténtalo de nuevo.';
+        }
+        showToast(context, errorMessage, toastType: ToastType.error);
+      }
+    } catch (e) {
+      if (mounted) {
+        showToast(context, 'Error: ${e.toString()}',
+            toastType: ToastType.error);
+      }
+    }
+  }
+
+  Future<void> _showResetPasswordDialog() async {
+    final TextEditingController newPasswordController = TextEditingController();
+    final TextEditingController confirmPasswordController =
+        TextEditingController();
+
+    await showDialog(
+      context: context,
+      barrierDismissible: false,
+      builder: (context) => AlertDialog(
+        title: const Text('Restablecer Contraseña'),
+        content: SingleChildScrollView(
+          child: Column(
+            children: [
+              TextFormField(
+                controller: newPasswordController,
+                decoration: const InputDecoration(
+                  hintText: 'Nueva Contraseña',
+                  border: OutlineInputBorder(),
+                ),
+                obscureText: true,
+              ),
+              const SizedBox(height: 16),
+              TextFormField(
+                controller: confirmPasswordController,
+                decoration: const InputDecoration(
+                  hintText: 'Confirmar Nueva Contraseña',
+                  border: OutlineInputBorder(),
+                ),
+                obscureText: true,
+              ),
+            ],
+          ),
+        ),
+        actions: [
+          TextButton(
+            onPressed: () {
+              if (newPasswordController.text ==
+                  confirmPasswordController.text) {
+                _resetPassword(newPasswordController.text);
+                Navigator.pop(context);
+              } else {
+                showDialog(
+                  context: context,
+                  builder: (context) => AlertDialog(
+                    title: const Text('Error'),
+                    content: const Text('Las contraseñas no coinciden.'),
+                    actions: [
+                      TextButton(
+                        onPressed: () => Navigator.pop(context),
+                        child: const Text('OK'),
+                      ),
+                    ],
+                  ),
+                );
+              }
+            },
+            child: const Text('Restablecer'),
+          ),
+        ],
+      ),
+    );
+  }
+
+  Future<void> _resetPassword(String newPassword) async {
+    try {
+      User? user = _auth.currentUser;
+      await user?.updatePassword(newPassword);
+      if (mounted) {
+        showDialog(
+          context: context,
+          builder: (context) => AlertDialog(
+            title: const Text('Contraseña Restablecida'),
+            content:
+                const Text('Tu contraseña ha sido restablecida exitosamente.'),
+            actions: [
+              TextButton(
+                onPressed: () => Navigator.pop(context),
+                child: const Text('OK'),
+              ),
+            ],
+          ),
+        );
+      }
+    } catch (e) {
+      if (mounted) {
+        showDialog(
+          context: context,
+          builder: (context) => AlertDialog(
+            title: const Text('Error'),
+            content: Text(e.toString()),
+            actions: [
+              TextButton(
+                onPressed: () => Navigator.pop(context),
+                child: const Text('OK'),
+              ),
+            ],
+          ),
+        );
+      }
+    }
   }
 
   @override
@@ -87,10 +246,8 @@ class _ForgotPasswordScreenState extends State<ForgotPasswordScreen>
                     ),
                     const SizedBox(height: 16),
                     ElevatedButton(
-                      onPressed: () {
-                        // Handle password reset logic
-                      },
-                      child: Text('Restablecer contraseña'),
+                      onPressed: _sendPasswordResetEmail,
+                      child: const Text('Restablecer contraseña'),
                     ),
                   ],
                 ),
